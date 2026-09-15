47f0835d01413757833dbbfaab34fa3dbbcb99d8
Guilherme-GMR <273337@unifio.edu.br>
2026-09-15 01:39:09 +0000

    aula 3

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+-- Active: 1789434563570@@127.0.0.1@3306@ecommerce
+insert into categoria (nome, descricao) values ('Livros', 'Tecnicos');
+
+insert into categoria (nome, descricao) values ('Eletronicos', 'Equipamentos Eletronicos');
+
+insert into categoria (nome, descricao) values ('Escritorio', 'Material de escritorio');
+
+insert into 
